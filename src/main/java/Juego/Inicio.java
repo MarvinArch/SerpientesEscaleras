51c12ecd739f25d89eb9d24d77b6a305f3206096
Marvin <marvinejo@gmail.com>
2021-06-25 20:34:08 -0600

    Pantalla Inicial

---
+++ src/main/java/Juego/Inicio.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Juego;
+
+/**
+ *
+ * @author alpha
+ */
+public class Inicio {
+    Inicial inicio= new Inicial();
+    public static void main(String[] args) {
+        Inicio var= new Inicio();
+    }
+    
+    public Inicio(){
+        inicio.setVisible(true);
+        //alalallala
+        
+    }
+}
